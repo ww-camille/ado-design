940e620d199b388122b2e59bacfb128c6db3fe27
Anna <annadonlin@gmail.com>
2018-03-20 12:57:12 -0700

    Manual File Upload
    VS to Git is not working...arg.

---
+++ Model/DbBuilder.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;   namespace portfolio.Model {     public class DbBuilder : DbContext     {
+        // DB-RELATED: ADD THESE CONTRUCTORS!
+        public DbBuilder() { }         public DbBuilder(DbContextOptions<DbBuilder> options) : base(options) { }
+
+        // DB-RELATED: CREATE A DB FOR EACH EXISTING MODEL(S)
+        //public DbSet<MyContact> Friends { get; set; }
+        //public DbSet<MyContact> Frenemies { get; set; }
+        //public DbSet<MyContact> Enemies { get; set; }
+        public DbSet<MyContact> MyContact { get; set; }        } }  
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -8,14 +8,76 @@ using Microsoft.Azure.KeyVault.Models;
 
 namespace portfolio.Pages
 {
+
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        // BRIDGE TO GREETINGS MODEL
+        [BindProperty]
+        public Model.MyContact bridgeMyContact { get; set; }
 
+        // DB-RELATED: CONNECT MY DATABASE TO THIS MODEL
+        private DbBuilder _myDB;
+        public IndexModel(DbBuilder myDB)
+        {
+            _myDB = myDB;
         }
 
-        // BRIDGE TO (GREETINGS) CONTACT MODEL
-        [BindProperty]         public Contact BridgeContact { get; set; }
+        public void OnGet(int ID = 0)
+        {
+            if (ID > 0)
+            {
+                bridgeMyContact = _myDB.MyContact.Find(ID);
+            }
+        }
+
+        // EMAIL-RELATED
+        public string Message { get; set; }
+        public IActionResult OnPost(int id = 0)
+        {
+            if (id > 0)
+            {
+                bridgeMyContact = _myDB.MyContact.Find(id);
+
+                try
+                {
+                    // SEND 
+                    MailMessage Mailer = new MailMessage();
+
+                    Mailer.Subject = bridgeMyContact.Subject;
+                    Mailer.Body = bridgeMyContact.Message;
+                    Mailer.From = new MailAddress(bridgeMyContact.Email, bridgeMyContact.Name);
+
+                    Mailer.IsBodyHtml = true;
+
+                    using (SmtpClient smtpServer = new SmtpClient())
+                    {
+                        smtpServer.EnableSsl = true;
+                        smtpServer.Host = "smtp.ado-design.com"; // CHANGE
+                        smtpServer.Port = 143; // CHANGE
+                        smtpServer.UseDefaultCredentials = false;
+                        smtpServer.Send(Mailer);
+                    }
+
+                    // DB-RELATED: ASSIGN SEND INFO TO DATABASE
+                    bridgeMyContact.sendDate = DateTime.Now.ToString();
+                    bridgeMyContact.sendIP = this.HttpContext.Connection.RemoteIpAddress.ToString();
+
+
+                    // DB-RELATED: UPDATE RECORD ON THE DATABASE 
+                    _myDB.MyContact.Update(bridgeMyContact);
+                    _myDB.SaveChanges();
+
+
+                    return RedirectToPage("Thankyou");
+                }
+                catch
+                {
+                    Message = "Oops, your message was not sent.";
+                }
+            }
+
+            return Page();
+        }
+
     }
 }
