b6a1fdaadbe56a40567c787aa301ac0a5225cd0a
Camille <ww-camille@users.noreply.github.com>
2018-03-20 17:01:32 -0700

    Update Index.cshtml.cs
    - Changed namespace 
    
    - Right-clicked on each one with squiggly lines, then clicked on BULB.  :)

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Azure.KeyVault.Models;
+using portfolio.Model;
 
 namespace portfolio.Pages
 {
